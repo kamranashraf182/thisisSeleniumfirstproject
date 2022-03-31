e7f31103e0dfcb6c3b36c2c1ad252cec0335561f
admin <admin@DESKTOP-7JDDCQC>
2022-03-31 13:36:02 +0530

    first commit

---
+++ src/seleniumfirst/Demo.java
@@ -0,0 +1,20 @@
+package seleniumfirst;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+
+	
+		public static void main(String[] args) throws InterruptedException {
+			System.setProperty("webdriver.chrome.driver", "./software/chromedriver.exe");
+			WebDriver driver=new ChromeDriver();
+			driver.manage().window().maximize();
+			Thread.sleep(2000);
+			driver.get("https://www.google.com/");
+			
+
+
+	}
+
+}
